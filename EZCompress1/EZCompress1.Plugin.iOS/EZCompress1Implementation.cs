f59fc7c50dfa07d2827bd2e6b2bb60147a86c292
Raviteja Lingineni <rlingineni@live.com>
2016-03-08 14:05:45 -0600

    Modified original compresImage method
    Added option to scale an image, the interface declaration will need to be updated. But this is the code that allows for the image to be resized and compressed based on a scale factor.

--- EZCompress1/EZCompress1.Plugin.iOS/EZCompress1Implementation.cs
+++ EZCompress1/EZCompress1.Plugin.iOS/EZCompress1Implementation.cs
@@ -14,32 +14,60 @@ namespace EZCompress1.Plugin
   {
       /// <summary>
       /// Compress the provided image with quality between 1 and 100%. Returns a memorystream of the
-      /// newly compressed image. Returns in jpeg format.
+      /// newly compressed image. Returns in jpeg format. 
+      ///also added option to scale the image based on the size, it will divide the image proportionally by that facter to make a smaller
+      ///or larger image. Useful when uploading to the web or creating thumbnails
       /// </summary>
-      public MemoryStream compressImage(Stream _image, int _compressAmount)
-      {
-          if (_compressAmount < 1 || _compressAmount > 100)
-          {
-              System.Diagnostics.Debug.WriteLine("Compress amount must be between 1 and 100! Compression failed.");
-              return null;
-          }
+      public	Stream compressImage(Stream image, int compressAmount, int scaleFactor)
+		{
+			if (compressAmount < 1 || compressAmount > 100)
+			{
+				System.Diagnostics.Debug.WriteLine("Compress amount must be between 1 and 100! Compression failed.");
+				return null;
+			}
 
-          _image.Position = 0;
+			image.Position = 0;
 
-          using (var data = NSData.FromStream(_image))
-          {
-              float compressAmount = (float)(_compressAmount * .01);
 
-              var image = UIImage.LoadFromData(data);
 
-              var newResult = image.AsJPEG(compressAmount);
-              var finalStream = newResult.AsStream();
+			using (var data = NSData.FromStream(image))
+			{
+				
+					
+				float _compressAmount = (float)(compressAmount * .01);
 
-              finalStream.Position = 0;
-              
-              System.Diagnostics.Debug.WriteLine("Compression succeeded!");
-              return (MemoryStream)finalStream;
-          }
-      }
+				var _image = UIImage.LoadFromData(data);
+
+				var scaledImage = scaleToSize (_image, scaleFactor);
+
+				var newResult = scaledImage.AsPNG ();
+
+				var finalStream = newResult.AsStream();
+
+				finalStream.Position = 0;
+
+				System.Diagnostics.Debug.WriteLine("Compression succeeded!");
+				return finalStream;
+			}
+
+		}
+
+		public UIImage scaleToSize(UIImage src, int scaleFactor)
+		{
+
+			CGRect rect = new CGRect(0.0, 0.0, src.Size.Width/scaleFactor, src.Size.Height/scaleFactor);
+
+			UIGraphics.BeginImageContext (rect.Size);
+			UIGraphics.GetCurrentContext ();
+
+			src.Draw (rect);
+			src = UIGraphics.GetImageFromCurrentImageContext();
+
+			UIGraphics.EndImageContext ();
+
+
+			return src;
+			
+		}
   }
 }
